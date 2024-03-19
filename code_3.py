f97c06372c31176ed1864b0eb22a6832806da823
eremeydack <139877570+eremeydack@users.noreply.github.com>
2024-03-19 02:38:12 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+}
+    return data;
+}
+        console.log("Random Number: " + item);
+
+main();
+
+    const data = generateRandomData();
+    }
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+function generateRandomData() {
+    });
